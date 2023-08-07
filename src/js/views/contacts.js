d464dafa5e2c69b914f3c843b1520f5f65320bc6
Deucalino Marildo Muvuma <muvumafernando@gmail.com>
2023-08-07 20:23:40 +0000

    Some sstyleadded

--- src/js/views/contacts.js
+++ src/js/views/contacts.js
@@ -6,10 +6,12 @@ export const ContactList=()=>{
   const {store, actions}=useContext(Context);
     return ( 
        <div className="bg-dark">
-            <div className="bg-secondary bg-opacity 10% text-center">
+            <div className="bg-secondary bg-opacity 25 text-center">
         <h2 className="text-light">My Contacts</h2>
+        <input className="bg-dark bg-opacity-5 text-light" type="text"onChange={e=>setNewContact(e.target.value)}/>
+        <button type="submit" onClick={()=>{actions.addContact(newContact)}} className="btn btn-outline-success text-light">+<i className="fa-solid fa-address-book"></i>Add</button>
         {store.contactList.map((contact,index)=>(
-         <p>{contact.name}</p>
+          <p>{contact.name}</p>
         ))}
        </div>
        </div>
