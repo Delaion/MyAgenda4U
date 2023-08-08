c2073b211960457d148b185866b2aefe8297528d
Deucalino Marildo Muvuma <muvumafernando@gmail.com>
2023-08-08 22:52:07 +0000

    code set, but there have troubles in the server

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,5 +1,3 @@
-
-
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
@@ -15,24 +13,122 @@ const getState = ({ getStore, getActions, setStore }) => {
 					initial: "white"
 				}
 			],
-		
-			contactList: [
-				
-			             ],
-			contact:[
-
-			        ]
-		
+			contacts: []
 		},
 		actions: {
+			getContacts : async()=>{
+				try {
+					
+					const response = await fetch("https://playground.4geeks.com/apis/fake/contact/agenda/Jparedes");
+					// Verificar si la respuesta está en el rango de 200-299 (éxito)
+					if (!response.ok) {
+					  throw new Error('La solicitud no fue exitosa');
+					}
+				
+					// Analizar la respuesta como JSON y devolver los datos
+					const contacts = await response.json();
+					setStore({contacts});
+				  } catch (error) {
+					// Manejar errores
+					console.error('Error al obtener los datos:', error.message);
+					throw error;
+				  }
+			},
+
+			addContacts: async(newContact)=>{
+				
+				try {
+					// Realizar la solicitud Post usando 
+					
+		 		 	const requestOptions = {
+					method: 'POST',
+					headers: {
+					'Content-Type': 'application/json', // Indicamos que el cuerpo de la solicitud es JSON
+					 'Accept' : 'application/json',
+					},
+					body: JSON.stringify(newContact), // Convertimos el objeto a JSON y lo enviamos como cuerpo de la solicitud
+		  			};
+		 		 	const response = await fetch( "https://playground.4geeks.com/apis/fake/contact/",requestOptions);
+					
+					// Verificar si la respuesta está en el rango de 200-299 (éxito)
+					if (!response.ok) {
+					  throw new Error('La solicitud no fue exitosa');
+					}
+					else{
+					// Analizar la respuesta como JSON y devolver los datos
+					const contacts = getActions().getContacts();
+					setStore({contacts});
+					}
+
+				  } catch (error) {
+					// Manejar errores
+					console.error('Error al obtener los datos:', error.message);
+					throw error;
+				  }
+			},
+			
+			editContacts: async(editContact,id)=>{
+				try {
+					// Realizar la solicitud POST usando fetch
+					const requestOptions = {
+					  method: 'PUT',
+					  body : JSON.stringify(editContact),
+					  headers: {
+						'Content-Type': 'application/json', // Indicamos que el cuerpo de la solicitud es JSON
+					  },
+					};
+					const response = await fetch(`https://playground.4geeks.com/apis/fake/contact/${id}`,requestOptions);
+					// Verificar si la respuesta está en el rango de 200-299 (éxito)
+					if (!response.ok) {
+					  throw new Error('La solicitud no fue exitosa');
+					}
+					else{
+					// Analizar la respuesta como JSON y devolver los datos
+					const contacts = getActions().getContacts();
+					setStore({contacts});
+					}
+
+				  } catch (error) {
+					// Manejar errores
+					console.error('Error al obtener los datos:', error.message);
+					throw error;
+				  }
+			},
+
+			deleteContacts: async(id)=>{
+				try {
+					// Realizar la solicitud POST usando fetch
+					const requestOptions = {
+					  method: 'DELETE',
+					  headers: {
+						'Content-Type': 'application/json', // Indicamos que el cuerpo de la solicitud es JSON
+					  },
+					};
+					const response = await fetch(`https://playground.4geeks.com/apis/fake/contact/${id}`,requestOptions);
+					// Verificar si la respuesta está en el rango de 200-299 (éxito)
+					if (!response.ok) {
+					  throw new Error('La solicitud no fue exitosa');
+					}
+					else{
+					// Analizar la respuesta como JSON y devolver los datos
+					const contacts = getActions().getContacts();
+					setStore({contacts});
+					}
+
+				  } catch (error) {
+					// Manejar errores
+					console.error('Error al obtener los datos:', error.message);
+					throw error;
+				  }
+			},
 			// Use getActions to call a function within a fuction
 			exampleFunction: () => {
 				getActions().changeColor(0, "green");
 			},
-			loadSomeData: () => {	
-				fetch("https://jsonplaceholder.typicode.com/users/")
-				.then(response=>response.json())
-				.then(data => setStore({ contactList: data}))	
+			loadSomeData: () => {
+				/**
+					fetch().then().then(data => setStore({ "foo": data.bar }))
+				*/
 			},
 			changeColor: (index, color) => {
 				//get the store
@@ -47,23 +143,10 @@ const getState = ({ getStore, getActions, setStore }) => {
 
 				//reset the global store
 				setStore({ demo: demo });
-			},
-			addContact:(contact)=>{
-                 const store=getStore();
-				 const contactList=store.contactList.concat(contact);
-				 //Resset the store
-				 setStore({contactList:contactList});
-			},
-		
-		    deleteContact:(index)=>{
-				const store=getStore();
-				const contactList=store.contactList.filter((c,i)=>{
-					return index !==i
-				});
-				//Resset the store
-		    	setStore({contactList:contactList});
-		    },
-	},
+			}
+		}
+	};
 };
-}
+
 export default getState;
+
